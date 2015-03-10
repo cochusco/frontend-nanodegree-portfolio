21c5c505711f7ba102a2ab2b61deb24ac1b498d8
josue <josue@example.com>
2015-03-10 22:11:17 +0100

    add interactive modals

---
+++ js/resume.js
@@ -0,0 +1,12 @@
+	//callback
+	$(initModal);
+	// ad interactive modal content according image clicked
+	function initModal(){
+		$("#project1").on("show.bs.modal", function (event) {	  
+	  	var image = $(event.relatedTarget); // Button that triggered the modal	  		
+		var modal = $(this);
+		var modaImg = modal.find('.img-responsive') ;
+		modaImg.attr("src" , image.attr("src"));
+		modaImg.attr("alt" , image.attr("alt"));
+	});
+	}
