7867dfbc6608295fc18d7646284d9d9a548542b2
josue <josue@example.com>
2015-03-31 21:10:41 +0200

    delete trailing whitespaces

--- js/resume.js
+++ js/resume.js
@@ -2,8 +2,8 @@
 	$(initModal);
 	// ad interactive modal content according image clicked
 	function initModal(){
-		$("#project1").on("show.bs.modal", function (event) {	  
-	  	var image = $(event.relatedTarget); // Button that triggered the modal	  		
+		$("#project1").on("show.bs.modal", function (event) {
+	  	var image = $(event.relatedTarget); // Button that triggered the modal
 		var modal = $(this);
 		var modaImg = modal.find('.img-responsive') ;
 		modaImg.attr("src" , image.attr("src"));
